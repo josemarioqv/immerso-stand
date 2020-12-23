b42e77430925525b397b10b7e1dddeba4241ee3c
josemarioqv <josemarioqv@gmail.com>
2020-12-23 04:28:45 -0600

    3D image, background sound and some information.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import background from './assets/background3.jpeg'
+import music from './assets/music.mp3'
 import './App.css';
+import 'aframe';
+
 
 function App() {
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a-scene>
+        
+
+        <a-plane position="10 2 -8" rotation="0 -40 0" width="7" height="4" color="#654321"></a-plane>
+        <a-plane position="10 -3 -8" rotation="0 -40 0" width="1" height="10" color="#654321"></a-plane>
+
+        <a-plane id='Contactanos'
+          position="4 0 8" 
+          rotation="0 200 0" 
+          width="4" 
+          height="2" 
+          color="#654321"
+          text="value: CONTACTANOS; color:black; align:center"
+          scale="1 1 1"
+          >          
+        </a-plane>
+        <a-plane position="4 -3 8" rotation="0 200 0" width="1" height="5" color="#654321"></a-plane>
+
+
+        <a-plane position="11 0 8" rotation="0 225 0" width="4" height="2" color="#654321"></a-plane>
+        <a-plane position="11 -3 8" rotation="0 225 0" width="1" height="5" color="#654321"></a-plane>
+
+        <a-plane position="18 0 6" rotation="0 250 0" width="4" height="2" color="#654321"></a-plane>
+        <a-plane position="18 -3 6" rotation="0 250 0" width="1" height="5" color="#654321"></a-plane>
+
+
+        <a-camera  wasd-controls-enabled="false">
+        <a-sound src={music} autoplay="true"
+          position="0 0 0" loop="true" volume="0.7"></a-sound>
+        </a-camera>
+
+        <a-sky src={background} sound="src: #waves"></a-sky>
+      </a-scene>
     </div>
   );
 }
